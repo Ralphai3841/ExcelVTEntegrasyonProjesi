d14a7caa6d023e2113cabe0cf49b2f97ba5d60ec
ralpd <ralpd@LAPTOP-FB9086OP>
2023-10-15 00:15:48 +0300

    Add project files.

---
+++ ExcelVTEntegrasyonProjesi/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace ExcelVTEntegrasyonProjesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVTdenOku = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnVTdenOku
+            // 
+            this.btnVTdenOku.Location = new System.Drawing.Point(564, 196);
+            this.btnVTdenOku.Name = "btnVTdenOku";
+            this.btnVTdenOku.Size = new System.Drawing.Size(177, 58);
+            this.btnVTdenOku.TabIndex = 0;
+            this.btnVTdenOku.Text = "Veri Tabanından Oku ve Excele Yaz";
+            this.btnVTdenOku.UseVisualStyleBackColor = true;
+            this.btnVTdenOku.Click += new System.EventHandler(this.btnVTdenOku_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(57, 61);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(473, 316);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnVTdenOku);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVTdenOku;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ ExcelVTEntegrasyonProjesi/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelVTEntegrasyonProjesi
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti = new SqlConnection(@"Data Source=LAPTOP-FB9086OP;Initial Catalog=ExcelVTEntegrasyonu;Integrated Security=True");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnVTdenOku_Click(object sender, EventArgs e)
+        {
+
+            Microsoft.Office.Interop.Excel.Application excelUygulama = new Microsoft.Office.Interop.Excel.Application();
+            excelUygulama.Visible = true;
+            Microsoft.Office.Interop.Excel.Workbook workbook = excelUygulama.Workbooks.Add(System.Reflection.Missing.Value);
+            Microsoft.Office.Interop.Excel.Worksheet sayfa1 = workbook.Sheets[1];
+
+            string[] baslikler = { "Personel no", "Ad", "Soyad", "Semt", "Şehir" };
+            Microsoft.Office.Interop.Excel.Range range;
+            for (int i = 0; i < baslikler.Length; i++)
+            {
+                range = sayfa1.Cells[1, (1 + i)];
+                range.Value2 = baslikler[i];
+            }
+
+
+
+            try
+            {
+                baglanti.Open();
+                string sqlCumlesi = "SELECT PersonelNo, Ad, Soyad, Semt, Sehir FROM Personel ";
+                SqlCommand sqlKomut = new SqlCommand(sqlCumlesi, baglanti);
+                SqlDataReader sdr = sqlKomut.ExecuteReader();
+
+                int satir = 2;
+                while (sdr.Read())
+                {
+                    string pno = sdr[0].ToString();
+                    string ad = sdr[1].ToString();
+                    string soyad = sdr[2].ToString();
+                    string semt = sdr[3].ToString();
+                    string sehir = sdr[4].ToString();
+                    richTextBox1.Text = richTextBox1.Text + pno + "  " + ad + "  " + soyad + "  " + semt + "  " + sehir + "\n";
+
+                    range = sayfa1.Cells[satir, 1];
+                    range.Value2 = pno;
+                    range = sayfa1.Cells[satir, 2];
+                    range.Value2 = ad;
+                    range = sayfa1.Cells[satir, 3];
+                    range.Value2 = soyad;
+                    range = sayfa1.Cells[satir, 4];
+                    range.Value2 = semt;
+                    range = sayfa1.Cells[satir, 5];
+                    range.Value2 = sehir;
+                    satir++;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("SQL Query sırasında bir hata oluştu, Hata Kodu: SQLREAD01 \n" + ex.ToString());
+            }
+            finally
+            {
+                if(baglanti != null)
+                baglanti.Close();
+            }
+        }
+    }
+}
